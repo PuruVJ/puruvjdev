4c931fdae8156fcaa6156b48821261679127bb11
Puru Vijay <awesomepuruvj@gmail.com>
2020-06-09 12:43:14 +0530

    New post

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { LocationSegments, MatchResults, } from "@stencil/router";
+import { LocationSegments, MatchResults } from "@stencil/router";
 export namespace Components {
     interface AppHome {
     }
