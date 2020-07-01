9ea1d68330915a606c094f2dfe06e3756cc548a7
Puru Vijay <awesomepuruvj@gmail.com>
2020-07-02 00:18:57 +0530

    Updated google ping

--- scripts/ping-google-search.js
+++ scripts/ping-google-search.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 (async () => {
   console.log("---- Pinging Google with the new sitemap ----");
   await fetch(
-    "http://www.google.com/ping?sitemap=https://puruvjdev.now.sh/sitemap.xml"
+    "http://www.google.com/ping?sitemap=https://puruvj.dev/sitemap.xml"
   );
   console.log('---- Pinging Google Done ----')
 })();
