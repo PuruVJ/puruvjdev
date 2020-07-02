74c74b813604f0b971f54523ad0f1987b0732ae0
Puru Vijay <awesomepuruvj@gmail.com>
2020-07-02 12:59:41 +0530

    No dark mode

--- src/state.ts
+++ src/state.ts
@@ -1,14 +1,15 @@
 import { createStore } from "@stencil/store";
 
 export const { state: appState, onChange } = createStore({
-  theme: window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light",
+  // theme: window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   ? "dark"
+  //   : "light",
+  theme: "light",
 });
 
-onChange("theme", (theme) => {
-  const meta: HTMLMetaElement = document.querySelector(
-    'meta[name="theme-color"]'
-  );
-  meta.content = theme === "dark" ? "#222428" : "rgb(254, 254, 254)";
-});
+// onChange("theme", (theme) => {
+//   const meta: HTMLMetaElement = document.querySelector(
+//     'meta[name="theme-color"]'
+//   );
+//   meta.content = theme === "dark" ? "#222428" : "rgb(254, 254, 254)";
+// });
